b422ab091cab8cb7e9892ce63cd71a2c423ac395
Diana <d7@hotmail.es>
2019-08-22 18:51:12 +0200

    Updates

---
+++ precache-manifest.2c840321bb312db7602cd5f60e19e807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26131c8439565c52e1e67bcb4e792e4f",
+    "url": "/events-searcher-react/index.html"
+  },
+  {
+    "revision": "56c7bdd89049a0ba26a1",
+    "url": "/events-searcher-react/static/css/main.62cb1aec.chunk.css"
+  },
+  {
+    "revision": "2c5c2efb0c713563edbb",
+    "url": "/events-searcher-react/static/js/2.05d453e0.chunk.js"
+  },
+  {
+    "revision": "56c7bdd89049a0ba26a1",
+    "url": "/events-searcher-react/static/js/main.35407474.chunk.js"
+  },
+  {
+    "revision": "1753ff0ca4cf5acaebb4",
+    "url": "/events-searcher-react/static/js/runtime~main.1cac3e69.js"
+  }
+]);
