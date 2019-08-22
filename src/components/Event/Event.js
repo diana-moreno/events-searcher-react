2fdf3e4ac8a8e1839b337a0b68850666a43159db
Diana <d7@hotmail.es>
2019-08-22 17:01:49 +0200

    fix cards layout

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -3,23 +3,25 @@ import React from 'react';
 const Event = ({event}) => {
     let {text} = event.description;
     if(text) {
-      text.length > 250 ? text = text.substring(0, 250).concat('...') : text = null; //limitar a 250 carácteres.
+      text.length > 250 ? text = text.substring(0, 230).concat('...') : text = null; //limitar a 250 carácteres.
     }
   return (
-    <div className='uk-card uk-card-default'>
-      <div className='uk-card-media-top'>
-        {event.logo ? <img src={event.logo.url} alt={event.name} /> : null}
-      </div>
+    <div>
+      <div className='uk-card-small uk-card-default'>
+        <div className='uk-card-media-top'>
+          {event.logo ? <img src={event.logo.url} alt={event.name} /> : null}
+        </div>
 
-      <div className='uk-card-body'>
-        <h3 className='uk-card-title'>{event.name.text}</h3>
-        {text}
-      </div>
+        <div className='uk-card-body uk-flex uk-flex-column uk-flex-middle'>
+          <h3 className='uk-card-title'>{event.name.text}</h3>
+          <p>{text}</p>
+        </div>
 
-      <div className='uk-card-footer uk-card-media-bottom'>
-        <a target='_blank' rel='noopener noreferrer' href={event.url} className='uk-button uk-button-secondary'>
-          More information
-        </a>
+        <div className='uk-card-footer'>{/*uk-position-bottom*/}
+          <a target='_blank' rel='noopener noreferrer' href={event.url} className='uk-button uk-button-secondary'>
+            More information
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/EventsList/EventsList.js
+++ src/components/EventsList/EventsList.js
@@ -4,7 +4,7 @@ import Event from '../Event/Event';
 
 const EventsList = () => {
   return (
-    <div className='uk-child-width-1-3@m' uk-grid='true'> {/*uk-child-width-1-2@s*/}
+    <div className='uk-child-width-1-3@m' uk-grid='true' uk-height-match="target: .uk-card-body"> {/*uk-child-width-1-2@s*/}
       <EventsConsumer>
         {(value) => {
           console.log(value);
